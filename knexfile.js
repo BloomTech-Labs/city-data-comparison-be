b4832b0907c3ab41d87c2b5b5fe6f9ac3c45e033
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-05-18 18:52:26 -0400

    updates connection in knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,52 +1,58 @@
-require('dotenv').config();
-const pg = require('pg');
-pg.defaults.ssl = true
+require("dotenv").config();
+const pg = require("pg");
+pg.defaults.ssl = true;
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations",
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { directory: "./database/seeds" },
   },
   testing: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations",
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { directory: "./database/seeds" },
   },
   staging: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.RDS_USERNAME,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: "./database/migrations"
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: "./database/seeds"
-    }
+      directory: "./database/seeds",
+    },
   },
   production: {
     client: "pg",
-    connection: process.env.RDS_HOSTNAME,
+    connection: {
+      port: process.env.RDS_PORT,
+      host: process.env.RDS_HOSTNAME,
+      database: process.env.RDS_DB_NAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+    },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      directory: "./database/migrations"
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: "./database/seeds"
-    }
-  }
+      directory: "./database/seeds",
+    },
+  },
 };
